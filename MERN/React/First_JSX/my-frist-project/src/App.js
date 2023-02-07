1d120ced4f75668071b3df140724194e3f41cb3b
Nima Bazofti <nimabaz@yahoo.com>
2023-02-07 15:49:21 -0800

    first jsx app

---
+++ MERN/React/First_JSX/my-frist-project/src/App.js
@@ -0,0 +1,33 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1 className="App-greeting1">Hello Dojo!</h1>
+        <h1 className="App-greeting2">Things I need to do:</h1>
+
+        <ul className="App-list">
+          <li>Learn React</li>
+          <li>Climb Mt. Everest</li>
+          <li>Run a marathon</li>
+          <li>Feed the dogs</li>
+        </ul>
+
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+
+      </header>
+    </div>
+  );
+}
+
+export default App;
