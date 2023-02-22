bd5c56bbb329ced0d921e002e0b2c41825e796fb
NimaBaz <32779333+NimaBaz@users.noreply.github.com>
2023-02-22 15:07:29 -0800

    Socket_io practice assignment

---
+++ MERN/Socket_io/Socket_io_example/client/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import Chat from './views/Chat';
+
+function App() {
+
+  return (
+    <div className="main">
+
+      <Chat/>
+
+    </div>
+  );
+}
+
+export default App;
